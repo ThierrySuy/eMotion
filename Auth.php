674344640acc474ced39c0565394320e3911f8d9
Maxis91 <riesco.maxime@gmail.com>
2017-08-28 10:15:44 +0200

    test

---
+++ Auth.php
@@ -0,0 +1,28 @@
+<?php
+
+Class Auth{
+    static function isLogged(){
+        if(isset($_SESSION['Auth']) && isset($_SESSION['Auth']['mail']) && isset($_SESSION['Auth']['pass']))
+        {
+            extract($_SESSION['Auth']);
+            $bdd = new PDO('mysql:host=localhost;dbname=emotion;charset=utf8', 'root', '');
+            $reponse = $bdd->prepare("SELECT id_user FROM user WHERE mail=? AND pass=?");
+            $reponse->execute(array($mail, $pass));
+            // $donnees = $reponse->fetch();
+            if($reponse -> rowCount()>0){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
+    function Loggin(){
+
+    }
+}
+
+
+?>
