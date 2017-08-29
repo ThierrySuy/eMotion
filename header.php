565b38c6606f64c4dfd4907ef53b24dc9da759d3
Maxis91 <riesco.maxime@gmail.com>
2017-08-29 15:21:52 +0200

    Maxime

--- header.php
+++ header.php
@@ -66,6 +66,7 @@
                 if ($_SESSION['Auth']['role'] == 4) {
                     echo '<li style="margin-right:auto;"><a href="control_vehicule.php">Gérer les véhicules de location</a></li>';
                     echo '<li style="margin-right:auto;"><a href="control_loc.php">Gérer les locations</a></li>';
+                    echo '<li style="margin-right:auto;"><a href="vehicule_en_location.php">Véhicules en cours de location</a></li>';
                 }
                 echo "    
           <li><a href='profil.php?id=$id'>Mon compte</a></li>
