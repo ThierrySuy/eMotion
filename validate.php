53e731cd21bba5e0e69a8b445143aa671e823020
Maxis91 <riesco.maxime@gmail.com>
2017-08-30 12:29:42 +0200

    ConnexionValidate

--- validate.php
+++ validate.php
@@ -1,24 +1,15 @@
 <?php
+
 session_start();
 
-if(isset($_POST) && !empty($_POST['mail']) && !empty($_POST['pass']))
-{
-    if (isset($_POST['louer']) && ($_POST['louer'] == 2)) {
-        $numero_serie = $_POST["num"];
-        $lien = 'Location:../pages/reserv.php?id='.$numero_serie;
-    }
-
-    else {
-        $lien = 'Location:index.php';
-    }
+if (isset($_POST) && !empty($_POST['mail']) && !empty($_POST['pass'])) {
     $pass = $_POST['pass'];
     $mail = $_POST['mail'];
     $pass = $pass;
     $bdd = new PDO('mysql:host=localhost;dbname=emotion;charset=utf8', 'root', '');
     $reponse = $bdd->query("SELECT * FROM user WHERE mail='$mail' AND pass='$pass'");
 
-    if($reponse->rowCount()>0)
-    {
+    if ($reponse->rowCount() > 0) {
         $user = $reponse->fetch();
         $_SESSION['Auth'] = array(
             'mail' => $mail,
@@ -27,15 +18,12 @@ if(isset($_POST) && !empty($_POST['mail']) && !empty($_POST['pass']))
         );
         $_SESSION['id'] = $user['id_user'];
         header('Location:index.php');
-    }else{
+    } else {
         $_SESSION['badId'] = ' <strong id="message">Identifiants Inconnus</strong>';
         header('Location:connexion.php');
     }
-
-}else{
+} else {
     $_SESSION['badId'] = '<strong id="message">Veuillez saisir vos identifiants</strong>';
     header('Location:connexion.php');
 }
-
-
 ?>
